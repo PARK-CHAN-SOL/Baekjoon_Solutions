6e72477a6b5416554c03e26ddd63a823c7fceae8
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 18:22:52 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 128 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a, b, c, d, e, f;
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        f = Integer.parseInt(st.nextToken());
+        System.out.println((c*e - f*b)/(a*e - d*b)+" "+(c*d - f*a)/(b*d - e*a));
+    }
+    
+}
