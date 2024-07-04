b915da7d73aeeb5c2bb2306196f06767c85e0e1f
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 17:48:08 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    public static void main(String[] args) throws IOException {
+        int a = read();
+        int b = read();
+        int count = 0;
+        for(int i = 1; i <= a; i++){
+            if(a % i == 0) count++;
+            if(count == b) {
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+}
