b6345900e7ce8d6d5818156a8645836062eec337
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 18:09:02 +0900

    [Bronze V] Title: 개수 세기, Time: 148 ms, Memory: 15900 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
