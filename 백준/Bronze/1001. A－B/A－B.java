5ed8046a42dfb1d530c8766d21f655a6315dd220
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 17:10:06 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) throws Exception {
+        int a = System.in.read();
+        System.in.read();
+        int b = System.in.read();
+        System.out.print(a-b);
+    }
+}
