c4a4c1231656426f23a01f34ecb16732694ecc50
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 18:19:02 +0900

    [Silver V] Title: 최소공배수, Time: 132 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    static int readPosInt() throws IOException{
+        int c, n = System.in.read() & 15;
+        while((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+    static long stein(long a, long b){
+        long axb = a * b;
+        int exp = 0;
+        
+        while(((a|b)&1) == 0){
+                exp++;
+                a >>= 1;
+                b >>= 1;
+        }
+            
+        while(a != 0 && b != 0){
+            while((a&1) == 0) a >>= 1;
+            while((b&1) == 0) b >>= 1;
+            if(a > b) a = a - b;
+            else b = b - a;
+        }
+
+        if(a > b) return(axb>>exp)/a;
+        else return(axb>>exp)/b;
+        
+    }
+    
+    public static void main(String[] args) throws IOException{
+        System.out.print(stein(readPosInt(),readPosInt()));
+    }
+}
