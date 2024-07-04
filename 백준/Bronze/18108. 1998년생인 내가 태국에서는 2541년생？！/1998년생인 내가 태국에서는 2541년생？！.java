45dd48e7f46c3021a3d24751e2a4a4280eb22d83
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 18:21:30 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.print(year-543);
+        sc.close();
+    }
+}
