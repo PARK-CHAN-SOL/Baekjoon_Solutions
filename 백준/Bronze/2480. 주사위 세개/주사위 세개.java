b84cc77f9548f7381bb7aba6b3ba1dfab3a7cb77
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 17:46:28 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
-
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) throws Exception {
+        
         int[] eye = new int[3];
 
-        eye[0] = sc.nextInt();
-        eye[1] = sc.nextInt();
-        eye[2] = sc.nextInt();
+        eye[0] = System.in.read() & 15;
+        System.in.read();
+        eye[1] = System.in.read() & 15;
+        System.in.read();
+        eye[2] = System.in.read() & 15;
+        
         if(eye[0] == eye[1] && eye[1] == eye[2]){
             System.out.print(10000+eye[0]*1000);
+            
         }else if(eye[0] == eye[1] || eye[1] == eye[2]){
             System.out.print(1000+eye[1]*100);
+            
         }else if(eye[0] == eye[2]){
             System.out.print(1000+eye[2]*100);
+            
         }else{
             if(eye[0] <= eye[1]){
                 eye[0] = eye[1];
@@ -24,6 +27,5 @@ public class Main{
             }
             System.out.print(100*eye[0]);
         }
-        sc.close();
     }
 }
