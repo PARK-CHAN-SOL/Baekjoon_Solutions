309fba4631060affd8ea3b413de0d70ac05c0d04
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 17:14:08 +0900

    [Bronze II] Title: 단어의 개수, Time: 264 ms, Memory: 20092 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        bw.write(st.countTokens()+"");
+
+        bw.close();
+        br.close();
+    }
+}
