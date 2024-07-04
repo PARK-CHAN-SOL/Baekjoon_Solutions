321e6c857d23955921613f784876a8f7c040f2fa
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 18:18:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        int ch = System.in.read();
+        System.out.println(ch);
+    }
+}
