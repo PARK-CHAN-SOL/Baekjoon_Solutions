f1c581983b3f9457b891a841ba4daa6de2ea6b92
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 18:23:40 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
+		n = n * n * n;
+		System.out.println(n);
+		System.out.println(3);
+	}
+
+}
