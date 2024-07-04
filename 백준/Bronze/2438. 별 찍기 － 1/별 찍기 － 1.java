06c61a4370602f0826569005fc8031ec33f139c5
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-04 17:35:39 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(bf.readLine());
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                bw.write("*");
+            }
+            if (i != (n-1))
+            bw.write("\n");
+        }
+        
+        bw.close();
+    }
+}
