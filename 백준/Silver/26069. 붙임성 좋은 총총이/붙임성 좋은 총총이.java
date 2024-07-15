aabfce7edf7441d3a9cff976581680a25285ed0c
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-16 05:26:57 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 136 ms, Memory: 16336 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -7,14 +7,12 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         HashSet<String> hs = new HashSet<>();
+        hs.add("ChongChong");
         
         for(int i = 0; i < n; i++){
             String s = br.readLine();
             String[] sArr = s.split(" ");
-            
-            if(sArr[0].equals("ChongChong")) hs.add(sArr[0]);
-            else if(sArr[1]. equals("ChongChong")) hs.add(sArr[1]);
-            
+
             if(hs.contains(sArr[0])) hs.add(sArr[1]);
             else if(hs.contains(sArr[1])) hs.add(sArr[0]);
         }
