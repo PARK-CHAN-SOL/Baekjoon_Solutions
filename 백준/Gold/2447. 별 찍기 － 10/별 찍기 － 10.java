5311c06385a70d0365827db71935a8cae2013cc2
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-19 17:14:39 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 264 ms, Memory: 38688 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -4,49 +4,73 @@ import java.io.*;
 
 class Main {
 
-    static char[][] stars;
+    static String[] arr;
+    static int[] toPowTwo = new int [2188];
+    static String space = " ";
+    static StringBuilder spacesb = new StringBuilder();
+    static StringBuilder arrsb = new StringBuilder();
     static int n;
-
-    static void stars (int k) {
-        int kk = (k << 1);
-        int limit = kk + k;
-        if(k == n) return;       
+    static int[] idxs;
+    static int oper = 1;
+    
+    static void  cantorArr (int limit) {
+        if(limit == toPowTwo[n]) return;
+            
         for(int i = 0; i < limit; i++){
-            for(int j = 0; j < limit; j++){
-                if(i >= k &&  i < kk && j >= k && j < kk) stars[i][j] = ' ';
-                else if(i >= k && i < kk && j < k) stars[i][j] = stars[i-k][j];
-                else if(i >= kk && i < limit && j < k) stars[i][j] = stars[i-kk][j];
-                else if(i < k && j >= k && j >= k && j < kk) stars[i][j] = stars[i][j-k];
-                else if(i >= kk && i < limit && j >= k && j < kk) stars[i][j] = stars[i-kk][j-k];
-                else if(i < k && j >= kk && j >= kk && j < limit) stars[i][j] = stars[i][j-kk];
-                else if(i >= k && i < kk && j >= kk && j < limit) stars[i][j] = stars[i-k][j-kk];
-                else if(i >= kk && i < limit && j >= kk && j < limit) stars[i][j] = stars[i-kk][j-kk];
-            }
+            arrsb.setLength(0);
+            arrsb.append(arr[i]).append(space).append(arr[i]);
+            arr[i + limit] = arrsb.toString();
+            arrsb.setLength(0);
+            arrsb.append(arr[i]).append(arr[i]).append(arr[i]);
+            arr[i] = arrsb.toString();
         }
+        spacesb.setLength(0);
+        spacesb.append(space).append(space).append(space);
+        space = spacesb.toString();
 
-        stars(limit);
-        
+        cantorArr(limit<<1);
     }
 
-    static int readPosInt() throws Exception {
+    static void setIdxs(int k) {
+        if(k == n) return;
+        int kk = k << 1;
+        for(int i = 0; i < k; i++){
+            idxs[i+k] = idxs[i]+oper;
+            idxs[i+kk] = idxs[i];
+        }
+        oper <<= 1;
+        setIdxs(kk + k);
+    }
+    
+    static int readPosInt () throws Exception {
         int c, n = System.in.read() & 15;
         while((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
-    
-    public static void main(String[] args) throws Exception  {
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        toPowTwo[3] = 2;
+        toPowTwo[9] = 4;
+        toPowTwo[27] = 8;
+        toPowTwo[81] = 16;
+        toPowTwo[243] = 32;
+        toPowTwo[729] = 64;
+        toPowTwo[2187] = 128;
+        
         n = readPosInt();
-        stars = new char[n][n];
+        arr = new String[toPowTwo[n]];
+        idxs = new int[n];
         
-        stars[0][0] ='*';
+        arr[0] = "*";
         
-        stars(1);
+        cantorArr(1);
+        setIdxs(1);
+        
+        arrsb.setLength(0);
 
         for(int i = 0; i < n; i++){
-            sb.append(stars[i]).append("\n");
+            arrsb.append(arr[idxs[i]]).append("\n");
         }
-        
-        System.out.print(sb);
+
+        System.out.print(arrsb);
     }
 }
