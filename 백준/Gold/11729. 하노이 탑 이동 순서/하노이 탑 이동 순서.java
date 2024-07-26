1d65a7e12bf3e2d4e9648dc9b88eb184b0d86e0d
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-26 13:50:11 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 296 ms, Memory: 65416 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,47 +1,38 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
 
 class Main {
     
     static int readPosInt() throws Exception {
         int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        while((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
-
+    
     public static void main(String[] args) throws Exception {
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int n = readPosInt();
-        int count = (1 << n) - 1;
-
-        char[] seq = "1 3\n".toCharArray();
-        char[] tmp;
-
-        for (int i = 1; i < n; i++) {
-            tmp = new char[seq.length * 2 + 4];
-            int ptr = 0;
-
-            for (int j = 0; j < seq.length; j++) {
-                if (seq[j] == '3') tmp[ptr++] = '2';
-                else if (seq[j] == '2') tmp[ptr++] = '3';
-                else tmp[ptr++] = seq[j];
-            }
-            tmp[ptr++] = '1';
-            tmp[ptr++] = ' ';
-            tmp[ptr++] = '3';
-            tmp[ptr++] = '\n';
-
-            for (int j = 0; j < seq.length; j++) {
-                if (tmp[j] == '3') tmp[ptr++] = '1';
-                else if (tmp[j] == '2') tmp[ptr++] = '3';
-                else if (tmp[j] == '1') tmp[ptr++] = '2';
-                else tmp[ptr++] = seq[j];
-            }
-
-            seq = tmp;
+        
+        int count = (int)Math.pow(2,n)-1;
+        
+        String s = "1 3\n";
+        
+        for(int i = 1; i < n; i++){
+            String tmp = s.replace('3','4');
+            tmp = tmp.replace('2','3');
+            tmp = tmp.replace('4','2');
+            sb.append(tmp).append("1 3\n");
+            tmp = tmp.replace('3','4');
+            tmp = tmp.replace('2','3');
+            tmp = tmp.replace('1','2');
+            tmp = tmp.replace('4','1');
+            sb.append(tmp);
+            s = sb.toString();
+            sb.setLength(0);
         }
-
-        writer.write(count + "\n");
-        writer.write(seq);
-        writer.flush();
+        
+        sb.append(count).append("\n").append(s);
+        System.out.print(sb);
     }
 }
