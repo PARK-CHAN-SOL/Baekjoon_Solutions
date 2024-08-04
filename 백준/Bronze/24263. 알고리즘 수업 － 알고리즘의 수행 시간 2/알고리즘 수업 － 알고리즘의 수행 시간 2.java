722a408e5b33f6086858299dcd5740ad40d4fa45
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-08-05 05:48:25 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -1,13 +1,17 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-        System.out.println(n);
-        System.out.println("1");
-        br.close();
+    
+	static int read() throws IOException {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		return n;
 	}
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append(read()).append("\n1");
+		System.out.println(sb);
+	}
+
 }
