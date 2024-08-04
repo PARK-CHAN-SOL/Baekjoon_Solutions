1263dedd8b274e45dc704302e64815f4fcc4a87f
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-08-05 05:34:14 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -4,17 +4,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    
+    static int readPosInt() throws Exception {
+        int c, n = System.in.read() & 15;
+        while((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
 
-	public static void main(String[] args) throws IOException {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 StringTokenizer st = new StringTokenizer(br.readLine());
-		 int x = Integer.parseInt(st.nextToken());
-		 int y = Integer.parseInt(st.nextToken());
-		 int w = Integer.parseInt(st.nextToken());
-		 int h = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		 int x = readPosInt();
+		 int y = readPosInt();
+		 int w = readPosInt();
+		 int h = readPosInt();
         
-		 if(x + x - w > 0) x = w - x;
-		 if(y + y - h > 0) y = h - y;
+		 if((x << 1) - w > 0) x = w - x;
+		 if((y << 1) - h > 0) y = h - y;
 		 if(x - y > 0) {
 			 System.out.println(y);
 		 } else {
