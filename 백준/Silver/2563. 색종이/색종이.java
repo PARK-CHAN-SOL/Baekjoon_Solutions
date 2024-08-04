0f73b8064eefd62fab01a2c339dac7852e5328bc
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-08-05 04:40:18 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -7,7 +7,6 @@ class Main {
     static int read() throws IOException{
         int c, n = System.in.read() & 15;
         while((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13) System.in.read();
         return n;
     }
 
