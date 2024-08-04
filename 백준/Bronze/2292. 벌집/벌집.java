87f2ffd76183acd8ab863969f36a9bdba97b54ea
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-08-05 04:49:48 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -7,13 +7,12 @@ class Main {
     static int read() throws IOException {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13) System.in.read();
         return n;
     }
     
     public static void main(String[] args) throws IOException {
         int n = read() - 1;
-        double room = Math.ceil((3 + Math.sqrt(9.0 + 12.0 * n)) / 6.0);
+        double room = Math.ceil((3.0 + Math.sqrt(9.0 + 12.0 * n)) / 6.0);
         System.out.println((int)room);
     }
 }
