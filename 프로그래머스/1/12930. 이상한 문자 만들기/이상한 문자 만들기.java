419615d8739dbb63a655dac78fdb000997cd1a23
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-09-24 22:59:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,24 +1,16 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        char[] arr = s.toCharArray();
-        
-        int count = 0;
-        
-        int len = arr.length;
-        
-        for(int i = 0; i < len; i++){
-            if(arr[i] != ' '){
-                if((count++ & 1) == 0){
-                    if(arr[i] >= 'a') arr[i] -= 32;
-                } else if (arr[i] < 'a'){
-                    arr[i] += 32;
-                }
-            } else count = 0;
+        String answer;
+        answer = s.toUpperCase();
+        char[] chars = answer.toCharArray();
+
+        //앞문자가 대문자라면 소문자로 치환
+        for (int i = 1; i < chars.length; i++) {
+            if (62 <= chars[i - 1] && chars[i - 1] <= 90) {
+                chars[i] = Character.toLowerCase(chars[i]);
+            }
         }
-        
-        answer = String.valueOf(arr);
-        
+        answer = String.valueOf(chars);
         return answer;
     }
 }
