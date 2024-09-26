c074de475ed4602bc65e7f9ac8eb9d32d03c6404
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-09-26 21:29:16 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 203.48 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,19 +1,26 @@
 import java.util.*;
-class Solution {
-   
-    public int solution(int[] elements) {
-        Set<Integer> set = new HashSet<>();
 
-        for (int i=1; i<=elements.length; i++) {
-            for (int j=0; j<elements.length; j++) {
-                int sum = 0;
-                for (int k=j; k<j+i; k++) {
-                    sum += elements[k%elements.length];
-                }
+class Solution {
+    public int solution(int[] elements) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        int start = 0;
+
+        for(int i=0; i<elements.length; i++) {
+            int n = 1;
+            int idx = i;
+            int sum = 0;
+            while(n <= elements.length) {
+                sum += elements[idx++];
                 set.add(sum);
+                if(idx >= elements.length) 
+                    idx = 0;
+                n++;
             }
         }
 
-        return set.size();
+        answer = set.size();
+
+        return answer;
     }
 }
