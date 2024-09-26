d58127162b46a8758cf0135a4a47fd1f0f1d7708
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-09-26 21:27:51 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1530.32 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,30 +1,19 @@
-import java.util.HashSet;
+import java.util.*;
 class Solution {
-    public void setPut (HashSet<Integer> set, int[] arr, int limit, int depth){
-        if(limit < depth) return;
-        for(int i = 0; i < limit; i++){
-            int sum = 0;
-            for (int j = 0; j < depth; j++){
-                sum += arr[i+j];
-            }
-            set.add(sum);
-        }
-        setPut(set, arr, limit, depth+1);
-    }
+   
     public int solution(int[] elements) {
-        int len = elements.length;
-        
-        int[] circle = new int[len<<1];
-        
-        for(int i = 0; i < len; i++){
-            circle[i] = elements[i];
-            circle[i+len] = elements[i];
+        Set<Integer> set = new HashSet<>();
+
+        for (int i=1; i<=elements.length; i++) {
+            for (int j=0; j<elements.length; j++) {
+                int sum = 0;
+                for (int k=j; k<j+i; k++) {
+                    sum += elements[k%elements.length];
+                }
+                set.add(sum);
+            }
         }
-        
-        HashSet<Integer> set = new HashSet<>();
-        
-        setPut(set, circle, len, 1);
-        
+
         return set.size();
     }
 }
