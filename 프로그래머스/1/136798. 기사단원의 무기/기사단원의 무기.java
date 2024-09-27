285c0b90b853ade661a7dd45f3d6b77b7ad8f4b2
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-09-27 20:43:27 +0900

    [level 1] Title: 기사단원의 무기, Time: 8.72 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,22 +1,19 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        
-        for(int i = 1; i <= number; i++){
-            int count = 2;
-            int sqrtI = (int)Math.sqrt(i);
-            if(Math.sqrt(i) == sqrtI) count--;
-            
-            for(int j = 2; j <= sqrtI; j++){
-                if(i % j == 0) {
-                    count+=2;
-                }
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            
-            if(count > limit) answer += power;
-            else answer += count;
         }
-        
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
+        }
         return answer;
     }
 }
