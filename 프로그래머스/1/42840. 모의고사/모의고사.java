90fecfd09ba5985f298af37e705a3fe2c981fe25
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-09-28 00:18:34 +0900

    [level 1] Title: 모의고사, Time: 1.37 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,59 +1,28 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int len = answers.length;
-        
-        int p1 = 0;
-        int p2 = 0;
-        int p3 = 0;
-        
-        for(int i = 0; i < len; i++){
-            switch(i % 5){
-                case 0: if(answers[i] == 1) p1++; break;
-                case 1: if(answers[i] == 2) p1++; break;
-                case 2: if(answers[i] == 3) p1++; break;
-                case 3: if(answers[i] == 4) p1++; break;
-                case 4: if(answers[i] == 5) p1++; break;
-            }
-            switch(i % 8){
-                case 0: if(answers[i] == 2) p2++; break;
-                case 1: if(answers[i] == 1) p2++; break;
-                case 2: if(answers[i] == 2) p2++; break;
-                case 3: if(answers[i] == 3) p2++; break;
-                case 4: if(answers[i] == 2) p2++; break;
-                case 5: if(answers[i] == 4) p2++; break;
-                case 6: if(answers[i] == 2) p2++; break;
-                case 7: if(answers[i] == 5) p2++; break;
-            }
-            switch(i % 10){
-                case 0: if(answers[i] == 3) p3++; break;
-                case 1: if(answers[i] == 3) p3++; break;
-                case 2: if(answers[i] == 1) p3++; break;
-                case 3: if(answers[i] == 1) p3++; break;
-                case 4: if(answers[i] == 2) p3++; break;
-                case 5: if(answers[i] == 2) p3++; break;
-                case 6: if(answers[i] == 4) p3++; break;
-                case 7: if(answers[i] == 4) p3++; break;
-                case 8: if(answers[i] == 5) p3++; break;
-                case 9: if(answers[i] == 5) p3++; break;
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+
+        int[] hit = new int[3];
+        for(int i = 0; i < hit.length; i++) {
+            for(int j = 0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
             }
         }
-        
-        int max = Math.max(p1, Math.max(p2, p3));
-        
-        if(max == p1 && max == p2 && max == p3){
-            return new int[]{1, 2, 3};
-        } else if(max == p1 && max == p2){
-            return new int[]{1, 2};
-        } else if(max == p1 && max == p3){
-            return new int[]{1, 3};
-        } else if(max == p2 && max == p3){
-            return new int[]{2, 3};
-        } else if(max == p1){
-            return new int[]{1};
-        } else if(max == p2){
-            return new int[]{2};
-        } else {
-            return new int[]{3};
-        }
+
+        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < hit.length; i++)
+            if(max == hit[i]) list.add(i + 1);
+
+        int[] answer = new int[list.size()];
+        int cnt = 0;
+        for(int num : list)
+            answer[cnt++] = num;
+        return answer;
     }
 }
