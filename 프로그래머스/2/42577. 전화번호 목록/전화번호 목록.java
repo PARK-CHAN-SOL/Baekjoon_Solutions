cf727e5bb35d526cba81f96d0ee74b1004a20fcb
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-09-28 19:57:27 +0900

    [level 2] Title: 전화번호 목록, Time: 336.62 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,30 +2,14 @@ import java.util.HashSet;
 import java.util.Arrays;
 class Solution {
     public boolean solution(String[] phone_book) {
-        int len = phone_book.length;
-        int max = 0;
-        int min = 21;
-        
-        Arrays.sort(phone_book, (s1, s2)->{
-            return s1.length() - s2.length() == 0 ? s1.compareTo(s2) : s1.length() - s2.length();
-        });
-        
-        for(int i = 0; i < len; i++){
-            max = Math.max(max,phone_book[i].length());
-            min = Math.min(min,phone_book[i].length());
-        }
-        
-        HashSet<String> set = new HashSet<>();
-        for(int i = min; i < max; i++){
-            for(int j = 0; j < len; j++){ 
-                
-                if(phone_book[j].length() > i && set.contains(phone_book[j].substring(0,i))) return false;
-                else set.add(phone_book[j]);
-                             
+        Arrays.sort(phone_book);
+        boolean result = true;
+        for (int i=0; i<phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                result = false;
+                break;
             }
-            set.clear();
         }
-        
-        return true;
+        return result;
     }
 }
