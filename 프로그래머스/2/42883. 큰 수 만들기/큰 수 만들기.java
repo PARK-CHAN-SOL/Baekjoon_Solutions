a62064db15728a807b6632486e26e9e8c60fd033
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-10-02 21:54:47 +0900

    [level 2] Title: 큰 수 만들기, Time: 5978.11 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(String number, int k) {
         StringBuilder answer = new StringBuilder();
+        char[] arr = number.toCharArray();
         int len = number.length();
         
         //0 부터 k 번째 까지 가장 큰 수(문자)를 찾는다
@@ -10,8 +11,8 @@ class Solution {
         while(k < len){
             char max = '0';
             for(int i = idx; i <= k; i++){
-                if(max < number.charAt(i)) {
-                    max = number.charAt(i);
+                if(max < arr[i]) {
+                    max = arr[i];
                     idx = i+1;
                 }
             }
